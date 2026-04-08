7c77f193f43e8a73f9071c090e8da8c59ca6e6f7
Lisa <lisa.eunhye@gmail.com>
2026-04-08 14:30:36 -0400

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Lisa Yi on 2026-04-08.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
